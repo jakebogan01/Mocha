4682edb829fbe9362a4544a9c30a9cb1a06484da
Jake Bogan <jakebogan01@gmail.com>
2023-01-30 00:53:30 -0500

    mobile home - started header section

---
+++ resources/views/components/navigation/mobile-nav.blade.php
@@ -0,0 +1,6 @@
+<nav class="relative flex justify-between items-center z-10">
+    <x-logo></x-logo>
+    <div class="cursor-pointer">
+        <svg xmlns="http://www.w3.org/2000/svg" width="37.364" height="24.654" viewBox="0 0 37.364 24.654"><g transform="translate(-4.188 -8.688)"><path d="M6.188,25.313H39.552" transform="translate(0 6.029)" fill="none" stroke="#fff" stroke-linecap="round" stroke-miterlimit="10" stroke-width="4"/><path d="M6.188,18H39.552" transform="translate(0 3.014)" fill="none" stroke="#fff" stroke-linecap="round" stroke-miterlimit="10" stroke-width="4"/><path d="M6.188,10.688H39.552" fill="none" stroke="#fff" stroke-linecap="round" stroke-miterlimit="10" stroke-width="4"/></g></svg>
+    </div>
+</nav>
